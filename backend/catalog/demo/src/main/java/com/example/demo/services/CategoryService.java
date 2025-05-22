29421aa35fc715483d9b8b101c1767ebec145f0d
Marcus <marcusdevofc@gmail.com>
2025-05-22 17:35:22 -0300

    H2 database

---
+++ backend/catalog/demo/src/main/java/com/example/demo/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Category;
+import com.example.demo.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+}
