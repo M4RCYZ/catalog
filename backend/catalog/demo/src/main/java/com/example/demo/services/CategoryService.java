c15a3c3abfaf9b615c91767c6893fc5f2e250d46
Marcus <marcusdevofc@gmail.com>
2025-05-25 15:52:21 -0300

    Database seeding

--- backend/catalog/demo/src/main/java/com/example/demo/services/CategoryService.java
+++ backend/catalog/demo/src/main/java/com/example/demo/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.example.demo.services;
 
 import com.example.demo.entities.Category;
 import com.example.demo.repositories.CategoryRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional
     public List<Category> findAll(){
         return repository.findAll();
     }
